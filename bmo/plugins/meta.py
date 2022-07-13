567f666992e5d7f97e76be8be416d72654c9965d
sticks <83383474+st1xkz@users.noreply.github.com>
2022-07-13 18:49:45 +0000

    edited ping command

--- bmo/plugins/meta.py
+++ bmo/plugins/meta.py
@@ -1,14 +1,17 @@
 import time
 
+import alluka
+import hikari
 import tanjun
 
 meta = tanjun.Component(name="meta")
 
 
-
 @meta.with_slash_command
 @tanjun.as_slash_command("ping", "Shows bot's ping/latency")
-async def cmd_ping(ctx: tanjun.abc.Context) -> None:
+async def cmd_ping(
+    ctx: tanjun.abc.Context, client: alluka.Injected[hikari.GatewayBot]
+) -> None:
     start = time.perf_counter()
     message = await ctx.respond(
         f"Pong! 🏓 \n" f"Ws Latency: **{ctx.bot.heartbeat_latency * 1000:.0f}ms**"
