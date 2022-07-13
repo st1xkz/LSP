c36a65b506973109d9d47586cb9a64dd805b0112
sticks <83383474+st1xkz@users.noreply.github.com>
2022-07-13 17:14:09 +0000

    .

--- bmo/plugins/meta.py
+++ bmo/plugins/meta.py
@@ -5,13 +5,12 @@ import tanjun
 meta = tanjun.Component(name="meta")
 
 
-
 @meta.with_slash_command
 @tanjun.as_slash_command("ping", "Shows bot's ping/latency")
 async def cmd_ping(ctx: tanjun.abc.Context) -> None:
     start = time.perf_counter()
     message = await ctx.respond(
-        f"Pong! 🏓 \n" f"Ws Latency: **{ctx.client.heartbeat_latency * 1000:.0f}ms**"
+        f"Pong! 🏓 \n" f"Ws Latency: **{ctx.bot.heartbeat_latency * 1000:.0f}ms**"
     )
     end = time.perf_counter()
 
