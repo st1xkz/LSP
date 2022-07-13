b8881374816ef69b4c804e0c7f67d96122a5ed19
sticks <83383474+st1xkz@users.noreply.github.com>
2022-07-13 06:36:25 +0000

    added hikari

--- bmo/client.py
+++ bmo/client.py
@@ -1,26 +1,34 @@
 import os
+
 import hikari
 import tanjun
 import uvloop
 
-def build_bot() -> GatewayBot:
-    TOKEN = os.environ['TOKEN']
+
+def build_bot() -> hikari.GatewayBot:
+    TOKEN = os.environ["TOKEN"]
     bot = hikari.GatewayBot(TOKEN)
 
     make_client(bot)
 
     return bot
 
+
 def make_client(bot: hikari.GatewayBot) -> tanjun.Client:
     client = (
-        tanjun.Client.from_gateway_bot(
-            bot,
-            mention_prefix=True,
-            set_global_commands=[993565814517141514, 870013765071028285]
+        (
+            tanjun.Client.from_gateway_bot(
+                bot,
+                mention_prefix=True,
+                set_global_commands=[993565814517141514, 870013765071028285],
+            )
         )
-    ).add_prefix(".").load_modules("bmo.plugins.utilities")
+        .add_prefix(".")
+        .load_modules("plugins.utilities")
+    )
 
     return client
+
 
 if os.name != "nt":
     uvloop.install()
--- bmo/plugins/utilities.py
+++ bmo/plugins/utilities.py
@@ -2,10 +2,12 @@ import tanjun
 
 component = tanjun.Component()
 
+
 @component.with_slash_command
 @tanjun.as_slash_command("id", "find out your id")
 async def cmd_id(ctx: tanjun.abc.Context) -> None:
     await ctx.respond(f"Your user ID is {ctx.author.id}")
+
 
 @tanjun.as_loader
 def load(client: tanjun.abc.Client) -> None:
