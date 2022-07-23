a400265cabdb875261605343b2c72f8b4351d999
sticks <83383474+st1xkz@users.noreply.github.com>
2022-07-23 13:39:14 +0000

    Is it respond instead of send idk

--- bmo/errors.py
+++ bmo/errors.py
@@ -6,16 +6,16 @@ errors = tanjun.AnyHooks()
 
 @errors.with_on_error
 async def on_error(ctx: tanjun.abc.Context, exc: Exception) -> None:
-    acc = (690631795473121280, 994738626816647262)
+    acc = [690631795473121280, 994738626816647262]
     
     await ctx.respond(
-        f"Something went wrong during invocation of command `{ctx.command.name}.`")
+        f"Something went wrong during invocation of command `{ctx.command.name}`.")
     
     embed = hikari.Embed(
         title=f"An unexpected {type(exc).__name__} occurred",
         description=f"```py\n{str(exc)[:1950]}```"
     )
-    await acc.send(embed)
+    await acc.respond(embed)
 
 
 @errors.add_to_command
