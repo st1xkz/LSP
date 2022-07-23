28938377c58abbf2fe3c84b6b3276c5554749eaf
sticks <83383474+st1xkz@users.noreply.github.com>
2022-07-23 14:13:47 +0000

    Iterated users

--- bmo/errors.py
+++ bmo/errors.py
@@ -6,16 +6,16 @@ errors = tanjun.AnyHooks()
 
 @errors.with_on_error
 async def on_error(ctx: tanjun.abc.Context, exc: Exception) -> None:
-    acc = [690631795473121280, 994738626816647262]
-    
-    await ctx.respond(
-        f"Something went wrong during invocation of command `{ctx.command.name}`.")
-    
-    embed = hikari.Embed(
-        title=f"An unexpected {type(exc).__name__} occurred",
-        description=f"```py\n{str(exc)[:1950]}```"
-    )
-    await acc.respond(embed)
+    users = [690631795473121280, 994738626816647262]
+
+    await ctx.respond(f"Something went wrong during invocation of command `{ctx.command.name}`.")
+
+    for user in users:
+        embed = hikari.Embed(
+            title=f"An unexpected {type(exc).__name__} occurred",
+            description=f"```py\n{str(exc)[:1950]}```",
+        )
+        await user.send(embed)
 
 
 @errors.add_to_command
