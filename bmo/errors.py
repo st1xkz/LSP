f3d56ec88aeef3d5a3300bd9be0a5bcd8148ea89
sticks <>
2022-07-24 03:22:37 +0000

    .

--- bmo/errors.py
+++ bmo/errors.py
@@ -6,18 +6,15 @@ errors = tanjun.AnyHooks()
 
 @errors.with_on_error
 async def on_error(ctx: tanjun.abc.Context, exc: Exception) -> None:
-    users = ctx.cache.get_user(690631795473121280, 994738626816647262) # 1: main acc, 2: second acc
-
     await ctx.respond(
         f"Something went wrong during invocation of command `{ctx.command.name}`."
     )
-
-    for user in users:
-        embed = hikari.Embed(
-            title=f"An unexpected {type(exc).__name__} occurred",
-            description=f"```py\n{str(exc)[:1950]}```",
-        )
-        await users.send(embed)
+    
+    embed = hikari.Embed(
+        title=f"An unexpected {type(exc).__name__} occurred",
+        description=f"```py\n{str(exc)[:1950]}```",
+    )
+    await ctx.respond(embed)
 
 
 @errors.add_to_command
