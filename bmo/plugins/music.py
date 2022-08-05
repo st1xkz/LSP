f671f43d31029d19c7360d683a5abca6ad70177d
sticks <lucyabney101@gmail.com>
2022-08-05 18:48:15 +0000

    little mistakes

--- bmo/plugins/music.py
+++ bmo/plugins/music.py
@@ -1,15 +1,15 @@
 import hikari
 import tanjun
 
-music = tanjun.Component(name="meta")
+music = tanjun.Component(name="music")
 
 
 @music.with_slash_command
-@tanjun.as_slash_command("play", "Plays a song")
+@tanjun.as_slash_command("play", "Plays a song from URL")
 async def cmd_play(ctx: tanjun.abc.Context) -> None:
     ...
 
 
-@tanjun.is_loader
+@tanjun.as_loader
 def load(client: tanjun.abc.Client) -> None:
     client.add_component(music.copy())
