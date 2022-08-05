a089ae04e089d33e7099b79c60aad32137ea1d86
sticks <lucyabney101@gmail.com>
2022-08-05 19:57:16 +0000

    .

--- bmo/plugins/music.py
+++ bmo/plugins/music.py
@@ -1,13 +1,25 @@
 import hikari
 import tanjun
 
+from asyncio import sleep
+from songbird import ytdl
+from songbird.hikari import Voicebox
+
 music = tanjun.Component(name="music")
 
 
 @music.with_slash_command
+@tanjun.with_str_slash_option("URL", "the URL of the song")
 @tanjun.as_slash_command("play", "Plays a song from URL")
 async def cmd_play(ctx: tanjun.abc.Context) -> None:
-    ...
+    guild = ctx.get_guild()
+    user_voice = guild.get_voice_state(ctx.user)
+    vc = user_voice.channel_id
+    voice = await Voicebox.connect(ctx.client, ctx.guild_id, vc)
+
+    await ctx.respond("Connected!")
+    track_handle = await voice.play_source(await ytdl(ctx.option.URL))
+    track_handle.play()
 
 
 @tanjun.as_loader
