39ade19c141186962b203477404bce142d87178b
sticks <lucyabney101@gmail.com>
2022-08-06 13:46:22 +0000

    added error message

--- bmo/plugins/music.py
+++ bmo/plugins/music.py
@@ -12,9 +12,13 @@ music = tanjun.Component(name="music")
 @tanjun.with_str_slash_option("url", "the URL of the song")
 @tanjun.as_slash_command("play", "Plays a song from URL")
 async def cmd_play(ctx: tanjun.abc.Context, url: str) -> None:
+    assert ctx.guild_id is not None
+
     guild = ctx.get_guild()
     voice_state = guild.get_voice_state(ctx.author)
     vc = voice_state.channel_id
+    if voice_state is None:
+        await ctx.respond("Please connect to a voice channel first.")
     voice = await Voicebox.connect(ctx.client, ctx.guild_id, vc)
 
     track_handle = await voice.play_source(await ytdl(url))
