a543dbe64a66d8b33ab3f00045e6a57203dd0a2b
sticks <lucyabney101@gmail.com>
2022-08-06 13:26:19 +0000

    changed back to old code

--- bmo/plugins/music.py
+++ bmo/plugins/music.py
@@ -12,12 +12,10 @@ music = tanjun.Component(name="music")
 @tanjun.with_str_slash_option("url", "the URL of the song")
 @tanjun.as_slash_command("play", "Plays a song from URL")
 async def cmd_play(ctx: tanjun.abc.Context, url: str) -> None:
-    assert ctx.guild_id is not None
-    
-    vc_state = ctx.get_guild().get_voice_state(ctx.guild_id, ctx.author)
-    if vc_state is None:
-        await ctx.respond("You are not in a voice channel.")
-    voice = await Voicebox.connect(ctx.client, ctx.guild_id, ctx.channel_id)
+    guild = ctx.get_guild()
+    voice_state = guild.get_voice_state(ctx.author)
+    vc = voice_state.channel_id
+    voice = await Voicebox.connect(ctx.client, ctx.guild_id, vc)
 
     track_handle = await voice.play_source(await ytdl(url))
     track_handle.play()
