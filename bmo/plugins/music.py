958e6a10c766661a0f39cf1a13f54eb4546da309
sticks <lucyabney101@gmail.com>
2022-08-06 05:15:46 +0000

    .

--- bmo/plugins/music.py
+++ bmo/plugins/music.py
@@ -11,14 +11,14 @@ music = tanjun.Component(name="music")
 @music.with_slash_command
 @tanjun.with_str_slash_option("url", "the URL of the song")
 @tanjun.as_slash_command("play", "Plays a song from URL")
-async def cmd_play(ctx: tanjun.abc.Context) -> None:
+async def cmd_play(ctx: tanjun.abc.Context, url: str) -> None:
     guild = ctx.get_guild()
     user_voice = guild.get_voice_state(ctx.user)
     vc = user_voice.channel_id
     voice = await Voicebox.connect(ctx.client, ctx.guild_id, vc)
 
     await ctx.respond("Connected!")
-    track_handle = await voice.play_source(await ytdl(ctx.option.url))
+    track_handle = await voice.play_source(await ytdl(url))
     track_handle.play()
 
 
