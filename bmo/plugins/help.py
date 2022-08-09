3b638bb5251fdc9ba7726b6669d20bfbc3d2a50c
sticks <lucyabney101@gmail.com>
2022-08-09 20:36:55 +0000

    new help

---
+++ bmo/plugins/help.py
@@ -0,0 +1,13 @@
+import hikari
+import tanjun
+
+help = tanjun.Component(name="help")
+
+@help.with_slash_command
+@tanjun.as_slash_command("help", "Shows help about all or one specific command")
+async def custom_help(ctx: tanjun.abc.Context) -> None:
+    ctx.respond("this is help")
+
+@tanjun.as_loader
+def load(client: tanjun.abc.Client) -> None:
+    client.add_component(help.copy())
