28eec526e0112b4d2728aa7c6d8f6448f04e9411
sticks <lucyabney101@gmail.com>
2022-08-09 02:04:21 +0000

    idk

--- bmo/plugins/help.py
+++ bmo/plugins/help.py
@@ -8,7 +8,7 @@ help = tanjun.Component(name="help")
 @help.with_slash_command
 @tanjun.with_str_slash_option("obj", "Object to get help for", default=False)
 @tanjun.as_slash_command("help", "Shows help about all or one specific command")
-async def cmd_help(ctx: tanjun.abc.Context, obj: str = None) -> None:
+async def cmd_help(ctx: tanjun.abc.Context, obj: str) -> None:
     if obj is None:
         await ctx.respond("this is help")
 
