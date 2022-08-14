604ad682fe2d1ee0081fc33f2136c8623bd5cb7e
sticks <lucyabney101@gmail.com>
2022-08-14 16:50:24 +0000

    .

--- bmo/plugins/help.py
+++ bmo/plugins/help.py
@@ -29,7 +29,7 @@ async def custom_help(
 Find all the commands available on this panel.""",
             color=0x77F2F2,
         )
-        for component.name, cmds in info.items():
+        for component.name, cmds.name  in info.items():
             embed.add_field(name=component, value=f"`{cmds}`", inline=False)
         embed.set_author(
             name="DJ BMO • Help",
