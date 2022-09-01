ce6aa85660748cafb1817e327024a203c578e81f
sticks <lucyabney101@gmail.com>
2022-09-01 03:24:50 +0000

    bold field values

--- bmo/plugins/help.py
+++ bmo/plugins/help.py
@@ -32,7 +32,7 @@ Find all the commands available on this panel.""",
             color=0x77F2F2,
         )
         for component, cmds in info.items():
-            embed.add_field(name=f"{component.name}", value=f"`{cmds}`", inline=False)
+            embed.add_field(name=f"{component.name}", value=f"**`{cmds}`**", inline=False)
         embed.set_author(
             name="DJ BMO • Help",
             icon=bot_user.avatar_url or bot_user.default_avatar_url,
