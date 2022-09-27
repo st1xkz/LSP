bf83be3e88cea8befd8589c78292b4612e8c4592
sticks <lucyabney101@gmail.com>
2022-09-27 02:56:22 +0000

    fix welcome message

--- bmo/components/listeners.py
+++ bmo/components/listeners.py
@@ -1,12 +1,17 @@
+import alluka
 import hikari
 import tanjun
 
 listener = tanjun.Component(name="Listeners")
 
-
+# turn this into an event. make into `on_guild_join`
 @listener.with_slash_command
 @tanjun.as_slash_command("foo", "Bar")
-async def cmd_foo(ctx: tanjun.abc.Context) -> None:
+async def cmd_foo(
+    ctx: tanjun.abc.Context, bot: alluka.Injected[hikari.GatewayBot]
+) -> None:
+    bot_user = bot.get_me()
+
     embed = hikari.Embed(
         title="Beep Boop!",
         description="""Thank you for inviting me! Type `/` to see what I can do!
@@ -14,6 +19,7 @@ async def cmd_foo(ctx: tanjun.abc.Context) -> None:
 All configuration is done through `/settings`. If you need any help or support, feel free to contact the [**developer**](https://discord.com/users/690631795473121280).""",
         color=0x3E77EE,
     )
+    embed.set_thumbnail(bot_user.avatar_url or bot_user.default_avatar_url)
     await ctx.respond(embed=embed)
 
 
