28354e45db86b1509e1ac6f056efc2862ede698a
sticks <lucyabney101@gmail.com>
2022-09-27 00:37:51 +0000

    make test command

--- bmo/components/listeners.py
+++ bmo/components/listeners.py
@@ -7,7 +7,12 @@ listener = tanjun.Component(name="Listeners")
 @listener.with_slash_command
 @tanjun.as_slash_command("foo", "Bar")
 async def cmd_foo(ctx: tanjun.abc.Context) -> None:
-    ...
+    embed = hikari.Embed(
+        title="Beep Boop!",
+        description="Thank you for inviting me! Type `/` to see what I can do!",
+        color=0x3E77EE,
+    )
+    await ctx.respond(embed=embed)
 
 
 @tanjun.as_loader
