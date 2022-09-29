3d3e77c7be74e246502db5f4d31158323ba6aa30
sticks <lucyabney101@gmail.com>
2022-09-29 00:42:57 +0000

    .

--- bmo/components/events.py
+++ bmo/components/events.py
@@ -7,7 +7,6 @@ component = tanjun.Component()
 @component.with_listener(hikari.GuildJoinEvent)
 async def on_guild_join(event: hikari.GuildJoinEvent):
     guild = event.get_guild()
-    bot_user = event.client.get_me()
 
     for _, ch in guild.get_channels().items():
         if toolbox.calculate_permissions(member, ch) & hikari.Permissions.SEND_MESSAGES:
@@ -18,7 +17,8 @@ async def on_guild_join(event: hikari.GuildJoinEvent):
                     description="""Thank you for inviting me! Type `/` to see what I can do!
                     
 All configuration is done through `/settings`. If you need any help or support, feel free to contact the [**developer**](https://discord.com/users/690631795473121280).""",
-                ).set_thumbnail(bot_user.avatar_url or bot_user.default_avatar_url),
+                    color=0x3E77EE,
+                )
             )
         break
 
