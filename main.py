8473cb54a55b259a4c0dee52534a395dbdb111c4
sticks <lucyabney101@gmail.com>
2022-10-06 00:14:54 +0000

    add status back, install lavasnek_rs, and add test music commands

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import hikari
 from bmo.client import build_bot
 
 if __name__ == "__main__":
-    build_bot().run()
+    build_bot().run(
+        status=hikari.Status.ONLINE,
+        activity=hikari.Activity(
+            name="I think I am dying. But that's okay, BMO always bounces back!",
+            type=hikari.ActivityType.WATCHING,
+        ),
+    )
