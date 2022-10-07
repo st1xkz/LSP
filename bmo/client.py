0921704c540723bd675ca09f73f5c8108906c53b
sticks <lucyabney101@gmail.com>
2022-10-07 23:33:42 +0000

    revert back to og

--- bmo/client.py
+++ bmo/client.py
@@ -73,7 +73,7 @@ def make_client(bot: hikari.GatewayBot) -> tanjun.Client:
         """Event that triggers when the hikari gateway is ready."""
         builder = (
             lavasnek_rs.LavalinkBuilder(event.my_user.id, TOKEN)
-            .set_password(os.environ["LAVALINK_PWD"])
+            .set_password(os.environ["Password"])
             .set_start_gateway(False)
             # We set start gateway False because hikari can handle
             # voice events for us.
