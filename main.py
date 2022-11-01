28bcef727da43e46f215197527158f4d45617d8f
sticks <lucyabney101@gmail.com>
2022-11-01 02:12:28 +0000

    add custom status to run function

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import hikari
 from lsp.client import build_bot
 
 if __name__ == "__main__":
-    build_bot().run()
+    build_bot().run(
+        status=hikari.Status.IDLE,
+        activity=hikari.Activity(name="oh, my glob!", type=hikari.ActivityType.PLAYING),
+    )
