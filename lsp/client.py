35fe13f4ab1fe6f741796cae6ce7e404616ae7c4
sarthhh <shiva02939@gmail.com>
2022-11-03 03:29:55 +0530

    remove excess code

--- lsp/client.py
+++ lsp/client.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 
 import hikari
@@ -11,20 +13,23 @@ from lsp.errors import *
 load_dotenv()
 
 
+class Bot(hikari.GatewayBot):
+    def run(self, **kwargs) -> None:
+
+        super().run(
+            status=hikari.Status.IDLE,
+            activity=hikari.Activity(
+                name="Oh, my Glob!", type=hikari.ActivityType.PLAYING
+            ),
+        )
+
+
 def build_bot() -> hikari.GatewayBot:
-    TOKEN = os.getenv("TOKEN")
-    bot = hikari.GatewayBot(
-        TOKEN,
-        banner=None,
-        intents=hikari.Intents.ALL,
-    )
-
-    make_client(bot)
-
-    return bot
+    TOKEN = os.getenv("TOKEN", "")
+    return make_client(Bot(TOKEN, banner=None, intents=hikari.Intents.ALL))[1]
 
 
-def make_client(bot: hikari.GatewayBot) -> tanjun.Client:
+def make_client(bot: hikari.GatewayBot) -> tuple[tanjun.Client, hikari.GatewayBot]:
     client = tanjun.Client.from_gateway_bot(
         bot,
         declare_global_commands=True,
@@ -33,7 +38,7 @@ def make_client(bot: hikari.GatewayBot) -> tanjun.Client:
     client.load_modules("lsp.components.starboard")
     client.set_hooks(tanjun.AnyHooks().set_on_error(on_error))
 
-    return client
+    return client, bot
 
 
 if os.name != "nt":
