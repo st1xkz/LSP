15b0f2e23104506064118b0cf3038c73f9cf416a
sticks <lucyabney101@gmail.com>
2022-11-02 17:10:09 +0000

    print list(ctx.client.iter_slash_commands()

--- lsp/components/meta.py
+++ lsp/components/meta.py
@@ -111,7 +111,8 @@ Command Handler: **hikari-tanjun v{tanjun.__version__}**""",
     "source", "Displays link to the bot's GitHub or to a specific command"
 )
 async def source(ctx: tanjun.abc.Context, cmd: str) -> None:
-    print(ctx.client.iter_slash_commands)
+    print(ctx.client.iter_slash_commands())
+    print({list(ctx.client.iter_slash_commands())})
     cmd = [
         cmd
         for cmd in ctx.client.iter_slash_commands()
