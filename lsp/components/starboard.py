17a612a9162a36bb9f2639e0cc147777e6416a98
st1xkz <83383474+st1xkz@users.noreply.github.com>
2022-11-04 23:50:01 +0000

    Add starboard event

--- lsp/components/starboard.py
+++ lsp/components/starboard.py
@@ -3,10 +3,22 @@ import tanjun
 
 starboard = tanjun.Component()
 
+emoji = "⭐"
+min_reaction = 3
 
-@starboard.with_listener(hikari.ReactionAddEvent)
-async def on_reaction_create(event: hikari.ReactionAddEvent):
-    ...
+
+@starboard.with_listener(hikari.GuildReactionAddEvent)
+async def on_reaction_create(event: hikari.GuildReactionAddEvent):
+    if starboard.client.is_alive:
+        return
+    if not str(event.emoji_name) == "⭐":
+        return
+        
+    message = event.bot.cache.get_message(event.message_id) or await event.bot.rest.fetch_message(event.guild_id, event.message_id)
+    num_reaction = ([reaction for reaction in message.reactions if str(reaction.emoji) == event.emoji_name][0]).count
+    
+    if num_reaction == min_reaction:
+        event.app.rest.create_message("this message has been starred.")
 
 
 @tanjun.as_loader
