cb02602b0b21e6f800221b4dd59641263db9fe6b
st1xkz <83383474+st1xkz@users.noreply.github.com>
2022-11-05 00:51:47 +0000

    change client to bot

--- lsp/components/starboard.py
+++ lsp/components/starboard.py
@@ -9,16 +9,26 @@ min_reaction = 1
 
 @starboard.with_listener(hikari.GuildReactionAddEvent)
 async def on_reaction_create(event: hikari.GuildReactionAddEvent):
-    if starboard.client.is_alive:
+    if starboard.bot.is_alive:
         return
     if not str(event.emoji_name) == "⭐":
         return
-        
-    message = event.bot.cache.get_message(event.message_id) or await event.bot.rest.fetch_message(event.guild_id, event.message_id)
-    num_reaction = ([reaction for reaction in message.reactions if str(reaction.emoji) == event.emoji_name][0]).count
-    
+
+    message = event.bot.cache.get_message(
+        event.message_id
+    ) or await event.bot.rest.fetch_message(event.guild_id, event.message_id)
+    num_reaction = (
+        [
+            reaction
+            for reaction in message.reactions
+            if str(reaction.emoji) == event.emoji_name
+        ][0]
+    ).count
+
     if num_reaction == min_reaction:
-        event.app.rest.create_message(1035754257686728734, "this message has been starred.")
+        event.app.rest.create_message(
+            1035754257686728734, "this message has been starred."
+        )
 
 
 @tanjun.as_loader
