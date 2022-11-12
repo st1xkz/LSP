335b0bc9683cdaec9289fb18645acfa882336a97
sticks <lucyabney101@gmail.com>
2022-11-12 20:17:16 +0000

    remove update_presence listener

--- lsp/bot.py
+++ lsp/bot.py
@@ -25,14 +25,8 @@ async def on_starting(event: hikari.StartingEvent) -> None:
     bot.d.aio_session = aiohttp.ClientSession()
 
 
-@bot.listen()
-async def on_started(event: hikari.StartedEvent) -> None:
-    update_presence.start()
-
-
 bot.load_extensions_from("./lsp/plugins/", must_exist=True)
 
 
 if os.name != "nt":
     uvloop.install()
-
