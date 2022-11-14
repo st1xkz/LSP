125c99b668b8b838ff81300e594a2e9c08b8d754
sticks <lucyabney101@gmail.com>
2022-11-14 01:15:35 +0000

    .

--- lsp/plugins/starboard.py
+++ lsp/plugins/starboard.py
@@ -27,7 +27,7 @@ async def reaction_added(event: hikari.GuildReactionAddEvent) -> None:
     if num_reaction == min_reaction:
         await starboard.bot.rest.create_message(
             1035754257686728734,
-            "⭐ x#number!",
+            "⭐",
             embed=hikari.Embed(description="hi", color=0xFCD203),
         )
 
